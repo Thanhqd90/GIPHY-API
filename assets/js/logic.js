cf9a7140497c53b0d6ebd1a4dd6730fd8554a691
Thanhqd90 <Thanhq1990@gmail.com>
2018-09-17 06:57:45 -0500

    Obtained GIPHY API

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,23 @@
-$(document).ready(function () { // document.ready start
+$(document).ready(function() {
 
-}); // document.Ready end
+var queryUrl = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=jpnxSbDkR1m61R1felUvixI0YfuJ0T2s&limit=10";
+
+var giflink;
+
+$.ajax({
+    url: queryUrl,
+    method: 'GET',
+    }) //Returns information from remote sever and passes it as response in the annonymous function
+.then(function(response){ 
+    console.log(response);
+    
+    giflink = response.data[4].url;
+    $("#title").text(giflink);
+    loglink();
+});
+
+function loglink() {
+    console.log(giflink);
+    }
+
+});
